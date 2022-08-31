03967c97f4bb1ec31c56204572a3d9f86eeb52ce
Naing Aung Zaw <111653761+DevAdriam@users.noreply.github.com>
2022-09-01 02:42:03 +0630

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const getmenubtn = document.querySelector(".menubtn");
-const getmenuitems = document.querySelector(".menuitems");
-
-getmenubtn.addEventListener("click", () => {
-  getmenubtn.classList.toggle("active");
-  getmenuitems.classList.toggle("active");
-});
